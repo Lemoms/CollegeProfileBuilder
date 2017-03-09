ee43daf6d5fd659c08296f3f8fe9cbd2acbfb835
Leimomi H. Podell <lpodell7479@bg-mmcte-19.d214.org>
2017-03-09 09:30:13 -0600

    Initial Commit

---
+++ CollegeProfileBuilder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollegeProfileBuilder
+//
+//  Created by Leimomi H. Podell on 3/9/17.
+//  Copyright © 2017 Leimomi H. Podell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
